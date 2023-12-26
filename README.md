dbaf095cf98ee4a84171cdf7c8b48c0cb3008ba5
Hyeokjin <tyeirdoo06@gmail.com>
2023-12-26 10:55:47 +0900

    add gitignore, add readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # simple_api_wrapper_42
+
+## Before use
+
+- make .env with
+
+```
+FT_API_UID=
+FT_API_SECRET=
+```
+
+- Install jq.
+
+## Usage
+
+1. execute login.sh
+   This will grant your access token
+
+2. send api call with send.sh
+
+For example, GET /v2/users/:id, and id is hdoo,
+
+```
+bash send.sh users/hdoo
+```
+
+will do all the jobs.
--- login.sh
+++ login.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MY_UID=u-s4t2af-ae4c841927546d450a8707e490f227ede384988a7f5cd55afa3917c9cab01c11
-MY_SECRET=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
 
-curl -X POST --data "grant_type=client_credentials&client_id=$MY_UID&client_secret=$MY_SECRET" https://api.intra.42.fr/oauth/token > .token.json
+source .env
+
+curl -X POST --data "grant_type=client_credentials&client_id=$FT_API_UID&client_secret=$FT_API_SECRET" https://api.intra.42.fr/oauth/token > .token.json
